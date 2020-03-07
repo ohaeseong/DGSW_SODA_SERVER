738ca4dd977df61d7588399bf409a48047e0e0a9
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-03-07 10:36:05 +0900

    [ADD] member models profileImage

--- src/lib/Validate/member.js
+++ src/lib/Validate/member.js
@@ -3,9 +3,10 @@ const Joi = BaseJoi.extend(require('@hapi/joi-date'));
 
 exports.validateRegisterUser = async (body) => {
   const schema = Joi.object().keys({
-    email: Joi.string().email().required(),
+    memberId: Joi.string().email().required(),
     pw: Joi.string().required(),
     name: Joi.string().required(),
+    profileImage: Joi.string().allow(null),
     certification: Joi.boolean().required(),
   });
   // eslint-disable-next-line no-useless-catch
