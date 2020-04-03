96b22c3ad062b663668ce1e42307829c652ff08e
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-04-03 22:00:50 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -37,9 +37,9 @@ server.listen(port, () => {
 
 try {
   const option = {
-    ca: fs.readFileSync('/etc/letsencrypt/fullchain.pem'),
-    key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/privkey.pem'), 'utf8').toString(),
-    cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/cert.pem'), 'utf8').toString(),
+    ca: fs.readFileSync('/etc/letsencrypt/live/fullchain.pem'),
+    key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/privkey.pem'), 'utf8').toString(),
+    cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/cert.pem'), 'utf8').toString(),
   };
 
   HTTPS.createServer(option, app).listen(sslport, () => {
