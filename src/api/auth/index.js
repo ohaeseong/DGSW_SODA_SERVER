355bebf0f7225034deebe6c7bfbd1e277c43f094
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-04-05 09:01:10 +0900

    [HOTFIX] auth url

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -3,6 +3,7 @@ const authCtrl = require('./auth.ctrl');
 
 auth.post('/login', authCtrl.login);
 auth.post('/register/member', authCtrl.registerAccount);
+auth.post('/check/member_id', authCtrl.checkMemberId);
 auth.post('/find/id', authCtrl.findId);
 auth.post('/email', authCtrl.emailVerify);
 
